cc612ccdaebbe86d186adc14fa2de0f59cfc1c29
Caleb <jorge_el_bago@hotmail.com>
2022-11-07 14:55:14 -0600

    Java

---
+++ SRC/index.js
@@ -0,0 +1,13 @@
+const fn = () => {
+    const miString = "Soy feliz"
+    let b = miString + "Muy feliz"
+    console.log(b)
+}
+    
+
+fn()
+
+const clickeado = () => {
+    let a = "Puerco Araña"
+    console.log() 
+}
